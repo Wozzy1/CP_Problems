feb357b9d3af7ba5598643eb7ee13b77584fe276
Matt Sinnwell <diepw50@gmail.com>
2025-04-17 22:00:20 -0500

    init multiplication game

---
+++ multiplication_game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+bool stanWins() {
+    
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    vector<int> v(30);
+
+
+    return 0;
+}
